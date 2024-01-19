379f9c739c5e19d0aae84c7ff3d561e8e5aac112
Rely Ky <relyky@hotmail.com>
2024-01-19 10:43:03 +0800

    Authentication & Authorization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
 using N8BlazorServerAuth.Components;
+using N8BlazorServerAuth.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +9,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
+//# for BLAZOR COOKIE Auth
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddHttpContextAccessor();
+/// ※ 不要在前端元件使用 IHttpContextAccesser 因為 HttpContext 的狀態並非即時反應的，
+/// 成因是 SignalR 與 HTTP 是不同的通訊協定且原則上不能共存。
+/// 但 Blazor Server App 又支援 Cookie Authentication。
+/// 改用 AuthenticationStateProvider 機制取登入狀態。
+
+//## for BLAZOR COOKIE Auth
+/// ref → https://blazorhelpwebsite.com/ViewBlogPost/36
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+  options.CheckConsentNeeded = context => true;
+  options.MinimumSameSitePolicy = SameSiteMode.Strict;
+});
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(cfg =>
+    {
+      cfg.LoginPath = "/Accout/Login"; // default: /Accout/Login
+      cfg.Cookie.Name = ".N8BlazorServerAuth.Cookies"; //default:.AspNetCore.Cookies
+    });
+
+var app = builder.Build(); //--------------------------------------------------
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,6 +45,11 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+//# for BLAZOR COOKIE Auth
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
---
+++ Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace N8BlazorServerAuth.Services;
+
+internal class CustomAuthenticationStateProvider(IHttpContextAccessor _http) : AuthenticationStateProvider
+{
+  public override Task<AuthenticationState> GetAuthenticationStateAsync()
+  {
+    var userIdentity = _http.HttpContext?.User.Identity as ClaimsIdentity;
+    if (userIdentity == null)
+    {
+      var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+      return Task.FromResult(anonymous);
+    }
+
+    // Success
+    return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(userIdentity)));
+  }
+}
