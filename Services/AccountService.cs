ab1b03484bafa3a06a54e6d1000097d835dad1be
Rely Ky <relyky@hotmail.com>
2024-01-22 13:43:28 +0800

    Login UI

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -6,18 +6,42 @@ public class AccountService
 {
   internal AuthUser? Authenticate(string userCredential)
   {
-    if (userCredential != "user credential")
+    var parser = userCredential.Split(':');
+    string email = parser[0].Trim();
+    string password = parser[1].Trim();
+    bool rememberMe = "True".Equals(parser[2]);
+
+    if (password != "user credential")
       return null;
 
-    return new AuthUser
+    if(email == "smart@mail.server")
     {
-      UserId = "smart",
-      UserName = "郝聰明",
-      AuthGuid = Guid.NewGuid(),
-      IssuedUtc = DateTime.UtcNow,
-      ExpiresUtc = DateTime.UtcNow.AddMonths(5),
-      Roles = ["Admin"],
-      RememberMe = false,
-    };
+      return new AuthUser
+      {
+        UserId = "smart",
+        UserName = "郝聰明",
+        AuthGuid = Guid.NewGuid(),
+        IssuedUtc = DateTime.UtcNow,
+        ExpiresUtc = DateTime.UtcNow.AddMonths(5),
+        Roles = ["Admin"],
+        RememberMe = rememberMe,
+      };
+    }
+    else if(email == "beauty@mail.server")
+    {
+      return new AuthUser
+      {
+        UserId = "beauty",
+        UserName = "甄美麗",
+        AuthGuid = Guid.NewGuid(),
+        IssuedUtc = DateTime.UtcNow,
+        ExpiresUtc = DateTime.UtcNow.AddMonths(5),
+        Roles = ["Admin"],
+        RememberMe = rememberMe,
+      };
+    }
+
+    // FAIL!
+    return null;
   }
 }
